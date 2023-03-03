6c45724de00bf2991dbf1fa117f117bd00752bc7
halt23 <maassam495@gmail.com>
2023-03-03 17:08:29 +0100

    removeed keyring script

--- get-samos-keyring.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/halt23/samos_repo/blob/main/x86_64/samos-keyring-2.1-1-any.pkg.tar.zst
-sudo pacman -U --noconfirm --needed samos-keyring-2.1-1-any.pkg.tar.zst
